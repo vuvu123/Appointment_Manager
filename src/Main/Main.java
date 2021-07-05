950c17224405ad8d283659ec37e11b21a881f3fd
vuvu123 <vuvu123@gmail.com>
2021-07-05 08:42:56 -0700

    Connected MYSQL Db driver 8.0.22 with DBConnection.java Added README with mysqlDB driver info

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,6 @@
 package Main;
 
+import Utils.DBConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,6 +19,8 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        DBConnection.startConnection();
         launch(args);
+        DBConnection.closeConnection();
     }
 }
---
+++ src/Utils/DBConnection.java
@@ -0,0 +1,51 @@
+package Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    // JDBC URL parts
+    private static final String protocol = "jdbc";
+    private static final String vendorName = ":mysql:";
+    private static final String ipAddress = "//wgudb.ucertify.com:3306/";
+    private static final String dbName = "WJ07aEM";
+
+    // JDBC URL
+    private static final String jdbcURL = protocol + vendorName + ipAddress + dbName;
+
+    // Driver interface reference
+    private static final String MYSQLJDBCDriver = "com.mysql.cj.jdbc.Driver";
+    private static Connection conn = null;
+
+    private static final String username = "U07aEM";
+    private static final String password = "53688972450";
+
+    public static Connection startConnection() {
+        try {
+            Class.forName(MYSQLJDBCDriver);
+            conn = DriverManager.getConnection(jdbcURL, username, password);
+
+            System.out.println("Connection successful");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+
+    public static Connection getConnection() {
+        return conn;
+    }
+
+    public static void closeConnection() {
+        try {
+            conn.close();
+        } catch (Exception e) {
+            // do nothing
+        }
+    }
+
+}
